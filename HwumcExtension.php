2e8355b2604ee3a26433c7e5139fb2aecc83c1e0
Simon Walker <github@stwalkerster.co.uk>
2013-05-28 13:37:48 +0100

    initial commit

---
+++ HwumcExtension.php
@@ -0,0 +1,28 @@
+<?php
+// check for invalid entry point
+if(!defined("HMS")) die("Invalid entry point");
+
+class HwumcExtension extends Extension
+{
+	public function getExtensionInformation()
+	{
+		return array(
+			"name" => "HWUMC Core",
+			"gitviewer" => "https://gerrit.stwalkerster.co.uk/gitweb?p=siteframework/extensions/hwumc-core.git;a=tree;h=",
+			"description" => "An example extension.",
+			"filepath" => dirname(__FILE__),
+		);
+	}
+	
+	protected function autoload( $class )
+	{
+	
+	}
+	
+	protected function registerHooks()
+	{
+	
+	}
+	
+
+}
